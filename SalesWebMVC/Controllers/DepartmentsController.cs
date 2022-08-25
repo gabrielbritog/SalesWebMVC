66f43de89269842be97bc60289572b4cb30634f3
Gabriel <gabriel.britog@outlook.com>
2022-08-25 12:20:09 -0300

    Escrita de ID em DepartmentsController corrigida

--- SalesWebMVC/Controllers/DepartmentsController.cs
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -54,7 +54,7 @@ namespace SalesWebMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Department department)
+        public async Task<IActionResult> Create([Bind("ID,Name")] Department department)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace SalesWebMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Department department)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Department department)
         {
             if (id != department.ID)
             {

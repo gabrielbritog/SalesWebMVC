332fcf59f671e36fc161bf84ae83d2e0c7a3dcb9
Gabriel <gabriel.britog@outlook.com>
2022-08-30 11:01:58 -0300

    Tratamento de exceção para delete(integridade referencial)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -33,9 +33,14 @@ namespace SalesWebMVC.Services
         }
         public async Task RemoveAsync(int id)
         {
+            try { 
             var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
             await _context.SaveChangesAsync();
+            }catch(DbUpdateException e)
+            {
+                throw new integrityException("Can't delete seller because he/she has sales");
+            }
         }
         public async Task UpdateAsync(Seller obj)
         {
